8afba4fff5abcb0101f8ee75d4e30ed952154a89
hawkinchina <free_hawk@sina.com>
2021-12-16 20:22:49 +0800

    3 files modified

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -24,15 +24,28 @@ export class TodolistComponent implements OnInit {
   // 定义doAdd事件
   doAdd(event: any) {
     console.log(typeof event);
-    console.log(event);
+    console.log(event + 'keyCode:' + event.keyCode);
     console.log('target:' + event.target + ' value:' + event.target.value);
 
-    this.todolist.push({
-      title: this.keyword,
-      status: 0, //0:表示代办 1: 表示已经完成
-    });
+    //keyCode = 13, 代表回车键, 接受任务输入。
+    if (event.keyCode == 13) {
+      if (!this.keyword) {
+        alert('不能为空');
+        return;
+      }
 
-    this.keyword = ''; //清空输入框中的内容
+      this.todolist.push({
+        title: this.keyword,
+        status: 0, //0:表示代办 1: 表示已经完成
+      });
+
+      this.keyword = ''; //清空输入框中的内容
+    }
+  }
+
+  //删除事件
+  del(key: number) {
+    this.todolist.splice(key, 1);
   }
 
   constructor() {}
